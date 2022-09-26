ac35460fc7c41a229e2b675e1082f67b55124930
carlfre <carlfrederiknk@gmail.com>
2022-09-26 15:38:58 +0200

    Add problem b to main.py

---
+++ src/main.py
@@ -0,0 +1,113 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import  train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
+
+import regression_tools as rt
+
+
+
+def problem_b():
+    print("Problem b)")
+
+    #TODO fix seed
+
+    # Generating data
+    N = 1000
+    x = np.random.uniform(0, 1, N)
+    y = np.random.uniform(0, 1, N)
+    epsilon = np.zeros((N)) #TODO: add non-zero error 
+    z = rt.FrankeFunction(x, y) + epsilon
+
+    # Create design matrix and train-test-split
+    deg = 5 # degree of pollynomial
+    X = rt.create_X_polynomial(x, y, deg)
+    X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.25)
+
+    # Estimate beta
+    beta = rt.linreg(X_train, z_train)
+
+    # Evaluate MSE and R2
+    ztilde_train = X_train @ beta
+    ztilde_test = X_test @ beta
+
+    print(f"Linear regression with polynomial of degree {deg}:")
+    print("R2 - training")
+    print(rt.R2(z_train, ztilde_train))
+    print("MSE - training")
+    print(rt.MSE(z_train, ztilde_train))
+    print()
+    print("R2 - testing")
+    print(rt.R2(z_test, ztilde_test))
+    print("MSE - testing")
+    print(rt.MSE(z_test, ztilde_test))
+    print()
+
+    # Evaluate R2 and MSE for polynomials of degree up to 5
+    degreerange = 5
+    degrees = range(2, degreerange + 1)
+    MSE_arr, R2_arr, beta_arr = [], [], []
+
+    # Plotting fits for all polynomial orders between 2 and 5
+    for deg in degrees:
+        X = rt.create_X_polynomial(x, y, deg)
+        X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.25)
+        beta = rt.linreg(X_train, z_train)
+        beta_arr.append(beta)
+        ztilde_test = X_test @ beta
+
+        R2_arr.append(rt.R2(z_test, ztilde_test))
+        
+        MSE_arr.append(rt.MSE(z_test, ztilde_test))
+
+    # Plotting MSE and R2 for all polynomial orders between 2 and 5
+    plt.plot(degrees, MSE_arr)
+    plt.xlabel("Polynomial order")
+    plt.ylabel("Mean Square Error")
+    plt.title("Change in MSE with increasing polynomial order")
+    plt.savefig("plots/MSE.pdf")
+    plt.show()
+    plt.plot(degrees, R2_arr)
+    plt.xlabel("Polynomial order")
+    plt.ylabel("R2-score")
+    plt.title("Change in R2 with increasing polynomial order")
+    plt.savefig("plots/R2.pdf")
+    plt.show()
+    print("Generated MSE and R2 plots.")
+    
+    # Plotting parameters in beta for all polynomial orders between 2 and 5
+    plt.figure(figsize=(5, 5)) 
+    for i in range(degreerange):
+        plt.plot(degrees, [b[i] for b in beta_arr], label=f"Coefficient {i}")
+    plt.title("First four parameters in beta for increasing polynomial order")
+    plt.xlabel("Polynomial order")
+    plt.ylabel("Coefficient value")
+    plt.legend()
+    plt.savefig("plots/beta.pdf")
+    plt.show()
+    print("Generated parameter value plot.")
+
+    
+
+def problem_c():
+    print("Problem c)")
+
+def problem_d():
+    print("Problem d)")
+
+def problem_e():
+    print("Problem e)")
+
+def problem_f():
+    print("Problem f)")
+
+def problem_g():
+    print("Problem g)")
+
+def main():
+    problem_b()
+
+if __name__ == "__main__":
+    main()
